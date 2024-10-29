4065144abca1d60d751e6354fc5835c044c2250c
“AwaKareem00” <Awa.kareem00@gmail.com>
2024-10-28 20:24:22 -0500

    push

--- cypress/e2e/integration/05-assertions.cy.js
+++ cypress/e2e/integration/05-assertions.cy.js
@@ -250,6 +250,77 @@ describe("Assertions", () => {
       .and("be.visible")
     })
 
+    const exp = ['Apple', 'Microsoft', 'Tesla']
+
+    cy.get('#checkbox-button-group div').each(($el, index) => {
+      cy.wrap($el).find('label').should('have.text', exp[index])
+
+      cy.wrap($el).find('input').should('be.visible').and('be.enabled')
+    })
 
   });
+
+  it('Assertion Practices', () => {
+    /**
+     * 1. Go to https://techglobal-training.com/frontend
+     * 2. Navigate to 'Html Elements' card
+     *
+     * 3. From the "Text Inputs" section
+     * 4. Validate text input 1 and text input 2 is enabled
+     * 5. Validate text input 1 and text input 2 is not required
+     * 6. Enter your name and last name
+     */
+
+
+    const names = ['tech', 'global']
+
+    cy.get('[id^="text_input"]').each(($el, index) => {
+      cy.wrap($el).should('be.enabled')
+      .type(names[index])
+      .and('not.have.attr', 'required')
+    });
+
+    /**
+     * 1. Go to https://techglobal-training.com/frontend
+     * 2. Navigate to 'Html Elements' card
+     *
+     * 3. From the "Date Inputs" section
+     * 4. Validate date input 1 and date input 2 is enabled
+     * 5. Validate date input 1 and date input 2 is is not required
+     * 6. Enter dates for both date input 1 and date input 2
+     * 7. Validate value is changed to given dates.
+     */
+
+    const dates = ["11/20/2022", "11/20/2021"];
+
+    cy.get('[id^="date_input"]').each(($el, index) => {
+      cy.wrap($el)
+        .clear()
+        .type(`${dates[index]}{enter}`)
+        .should("have.value", dates[index])
+        .and("be.enabled")
+        .and("not.have.attr", "required");
+    });
+
+    /**
+     * 1. Go to https://techglobal-training.com/frontend
+     * 2. Navigate to 'Html Elements' card
+     *
+     * 3. From the "Dropdowns" section
+     * 4. Validate dropdown 1 and dropdown 2 is enabled
+     * 6. Enter Microsoft for dropdown 1 and Apple for dropdown 2
+     * 7. Validate options are selected
+     */
+
+    const drop = ["Microsoft", "Apple"];
+
+    cy.get('[id^="company_dropdown"]').each(($el, index) => {
+    cy.wrap($el)
+    .should("be.enabled")
+    .select(drop[index])
+    .should('have.value', drop[index]);
+    });
+
+  })
+  
 });
